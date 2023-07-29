1fb93afb8bc1507b36b69579b7eac2ba72a6f492
Maxime Daniel <maxime.daniel@adagp.fr>
2023-07-29 15:10:53 +0200

    Mock web server in tests

--- src/test/java/com/example/sseclient/SseWebClientIT.java
+++ src/test/java/com/example/sseclient/SseWebClientIT.java
@@ -1,23 +1,61 @@
 package com.example.sseclient;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
-
 import reactor.core.publisher.Flux;
 
+import java.io.IOException;
+import java.lang.reflect.Executable;
+import java.net.ConnectException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SseWebClientIT {
-	
-	private final WebClient webClient = WebClient.builder()
-			.baseUrl("http://localhost:3000")
-			.build();
+
+	private final MockWebServer mockWebServer = new MockWebServer();
+	private final SseWebClient<String> webClient = new SseWebClient<>(
+			WebClient.create(mockWebServer.url("/").toString()),
+			String.class
+	);
+
+	@AfterEach
+	void teardownMockServer() throws IOException {
+		mockWebServer.shutdown();
+	}
 
 	@Test
 	void shouldRetrieveDataFromServer() {
-		SseWebClient<String> sseWebClient = new SseWebClient<>(webClient, String.class);
-		Flux<String> times = sseWebClient.retrieveData("/events");
+		final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd"); // ISO 8601
+		final StringBuilder eventStreamBuilder = new StringBuilder();
+
+		// add 15 events
+		for (int i = 0; i < 15; i++) {
+			final String dateString = simpleDateFormat.format(new Date());
+			eventStreamBuilder
+					.append("event: newEvent\ndata: {\"event\":\"a new event occurred!\",\"eventDate\":\"")
+					.append(dateString)
+					.append("\"}\n\n");
+		}
+
+		mockWebServer.enqueue(
+				new MockResponse()
+						.setResponseCode(200)
+						.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_EVENT_STREAM)
+						.setBody(eventStreamBuilder.toString())
+		);
+
+		Flux<String> times = webClient.retrieveData("/events");
 		
 		assertNotNull(times);
 		times.take(5).count().subscribe((c) -> assertTrue(c > 0));
